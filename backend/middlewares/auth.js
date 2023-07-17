0d06d182368e5989ce1b4da5c2c8a53f903f69e6
IrinaSerpokrylova <ira.serpokrylova@yandex.ru>
2023-07-18 00:12:20 +0300

    change back

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,20 +1,20 @@
 const { NODE_ENV, JWT_SECRET } = process.env;
 
-const jwtCheck = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../utils/errors/unauthorized-error');
 
 module.exports = (req, res, next) => {
   const secretKey = NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret';
 
-  if (!req.cookies.jwt) {
+  if (!req.cookies.token) {
     throw new UnauthorizedError('Необходима авторизация');
   }
 
-  const { jwt } = req.cookies;
+  const { token } = req.cookies;
   let payload;
 
   try {
-    payload = jwtCheck.verify(jwt, secretKey);
+    payload = jwt.verify(token, secretKey);
   } catch (err) {
     throw new UnauthorizedError('Необходима авторизация');
   }
